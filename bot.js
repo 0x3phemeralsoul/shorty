3edaf3daf486de278d42ceb2eae7916116c71cd7
0x3phemeralsoul <a>
2024-09-01 09:42:14 +0200

    feat: addming comments

--- bot.js
+++ bot.js
@@ -13,19 +13,21 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 const DISCORD_CHANNEL_ID = process.env.DISCORD_CHANNEL_ID;
 const SHORTCUT_API_TOKEN = process.env.SHORTCUT_API_TOKEN;
-const LOGGER_LEVEL= process.env.LOGGER_LEVEL
+const LOGGER_LEVEL = process.env.LOGGER_LEVEL;
+const MAX_RETRIES = process.env.MAX_RETRIES;
+const RETRY_DELAY = process.env.RETRY_DELAY;
 
 // Configure winston for logging
 const logger = winston.createLogger({
-  level: LOGGER_LEVEL,
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => `${timestamp} [${level}]: ${message}`)
-  ),
-  transports: [
-    new winston.transports.Console(), // Logs to console
-    new winston.transports.File({ filename: 'app.log' }) // Logs to file
-  ]
+    level: LOGGER_LEVEL,
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => `${timestamp} [${level}]: ${message}`)
+    ),
+    transports: [
+        new winston.transports.Console(), // Logs to console
+        new winston.transports.File({ filename: 'app.log' }) // Logs to file
+    ]
 });
 
 // Function to handle login with retry mechanism
@@ -58,38 +60,49 @@ users.set('6380d4da-0ae7-4ca5-a7f4-59ba335b4460', '@1041280647655850064'); // 3p
 users.set('656f49bd-1c7a-42bb-874e-2a028317b06b', '@1157492699415457892'); // oddaf
 users.set('64d0f1b4-991e-4d95-b3fe-521a7cd53e04', '@1138099723807494295'); // p3th1um
 
+/* let nickname_users = new Map()
+nickname_users.set('0xdecr1pto8698', '@674714406218891327'); // decr1pto
+nickname_users.set('amusingaxl', '@593551141921357862'); // amusing
+nickname_users.set('0x3phemeralsoul', '@1041280647655850064'); // 3phy
+nickname_users.set('oddaf6975', '@1157492699415457892'); // oddaf
+nickname_users.set('0xp3th1um', '@1138099723807494295'); // p3th1um */
+
 // Endpoint to receive webhook events
 app.post('/', async (req, res) => {
     const event = req.body;
-    const primary_id = event.actions[0].id;
-    let in_review_workflow_state_id = 0
-
-    
-    if('changes' in event.actions[0]&& 'workflow_state_id' in event.actions[0].changes){
-        in_review_workflow_state_id = event.actions[0].changes.workflow_state_id.new
+    let story_id = 0;
+    for (action in event.actions) {
+        //checking the ID from the story, not from the comment or any other item, just the story
+        if(event.actions[action].entity_type == 'story' && event.actions[action].action == 'update' ){
+            story_id = event.actions[action].id
+        }
     }
+    
+    logger.info(`Received webhook event for story ID: ${story_id}`);
+    let in_review_workflow_state_id = 0;
 
-    if (in_review_workflow_state_id == 500000189){
-
-        logger.info(`Received webhook event for story ID: ${primary_id}`);
+    if ('changes' in event.actions[0] && 'workflow_state_id' in event.actions[0].changes) {
+        in_review_workflow_state_id = event.actions[0].changes.workflow_state_id.new;
+    }
+    // state change to in review
+    if (in_review_workflow_state_id == 500000189) {
+        
 
         try {
             // Fetch the story details using Shortcut API
-            const story = await axios.get(`https://api.app.shortcut.com/api/v3/stories/${primary_id}`, {
-            headers: {
-                "Shortcut-Token": SHORTCUT_API_TOKEN,
-                "Content-Type": "application/json"
-            }
+            const story = await axios.get(`https://api.app.shortcut.com/api/v3/stories/${story_id}`, {
+                headers: {
+                    "Shortcut-Token": SHORTCUT_API_TOKEN,
+                    "Content-Type": "application/json"
+                }
             });
-
             const story_data = story.data;
-            //console.log(story_data)
             const story_url = story_data.app_url;
             const story_name = story_data.name;
             const workflow_id = story_data.workflow_id;
             const workflow_state_id = story_data.workflow_state_id;
-            let deadline = null
-            if(story_data.deadline != null){
+            let deadline = null;
+            if (story_data.deadline != null) {
                 deadline = story_data.deadline.split("T", 1)[0];
             }
 
@@ -98,23 +111,23 @@ app.post('/', async (req, res) => {
 
                 let task_owners = story_data.tasks.map(task => task.owner_ids[0]);
                 let discord_mentions = task_owners.map(owner => `<${users.get(owner)}>`);
-                let last_pr = extractGitHubPullRequestUrls(getComments(story_data.comments))
-                
-                if (last_pr.length != 0){
-                    last_pr.slice(-1)[0]
+                let last_pr = extractGitHubPullRequestUrls(getComments(story_data.comments));
+
+                if (last_pr.length != 0) {
+                    last_pr = last_pr.slice(-1)[0];
                 }
                 logger.info(`Last PR is "${last_pr}"`);
-                let message = ''
-                let deadline_msg =  ''
-                let pr_msg = ''
-                if(deadline != null){
-                    deadline_msg= ` :bangbang:**Story Due Date** is ${deadline} :bangbang: so the review should be in sooner than that! :exploding_head:`;
+                let message = '';
+                let deadline_msg = '';
+                let pr_msg = '';
+                if (deadline != null) {
+                    deadline_msg = ` :bangbang:**Story Due Date** is ${deadline} :bangbang: so the review should be in sooner than that! :exploding_head:`;
                 }
-                if(last_pr != ''){
+                if (last_pr != '') {
                     pr_msg = ` Link to [PR](<${last_pr}>)`;
                 }
-                message = `[${story_name}](${story_url}) is ready for review by ${discord_mentions.join(', ')}`+pr_msg+deadline_msg;
-                
+                message = `[${story_name}](${story_url}) is ready for review by ${discord_mentions.join(', ')}` + pr_msg + deadline_msg;
+
                 const channel = await client.channels.fetch(DISCORD_CHANNEL_ID);
                 channel.send(message);
 
@@ -123,20 +136,92 @@ app.post('/', async (req, res) => {
                 res.status(200).send('Event processed');
             } else {
                 logger.info(`Story "${story_name}" does not meet the criteria for notification.`);
-                res.status(200).send('No action required');
+                res.status(200).send('No action required.');
             }
 
+        
         } catch (error) {
             logger.error('Error processing event:', error);
             res.status(500).send('Internal Server Error');
         }
-    }
+    } else{
+
+        // if the story is not in review state change, then it is a comment that is worth notifying, if the story is in review state change, the the Review message is more relevant than the comment cuz the comment is the PR and the PR will be in the Review discord message
+        // Check for new comments in the event
+        let story_name;
+        let story_url;
+        let story_id;
+        let comment_text;
+        let mention_ids;
+        let task_owners;
+        let discord_mentions = []
+        let is_comment_created = false;
+        event.actions.forEach(action => {
+            if (action.action == 'create' && action.entity_type == 'story-comment') {
+                is_comment_created = true;
+                comment_text = action.text;
+                console.log(action.hasOwnProperty('mention_ids'))
+                if(action.hasOwnProperty('mention_ids')){
+                mention_ids = action.mention_ids;
+                discord_mentions = mention_ids.map(owner => `<${users.get(owner)}>`);
+                }
+                
+            }
+            if (action.action == 'update' && action.entity_type == 'story') {  
+                story_url = action.app_url;
+                story_name = action.name;
+                story_id = action.id
+
+            }
+            
+            });
+            try {
+                
+                
+                //if the webhook event is due to a new comment AND there are users mentioned in the comment I mention the users mentioned in the comment.
+
+                if (comment_text != '' && discord_mentions.length > 0) {
+                    const comment_message = `${discord_mentions.join(', ')} New comment on [${story_name}](${story_url})`;
+                    const channel = await client.channels.fetch(DISCORD_CHANNEL_ID);
+                    channel.send(comment_message);
+    
+                    logger.info(`Sent message with mentions to Discord channel ${DISCORD_CHANNEL_ID}: ${comment_message}`);
+                    res.status(200).send('Event processed');
+                // if there is a new comment, but not users mentioned in the comment I mention the "owner" of the ticket
+                }else if (comment_text != '' && is_comment_created){
+                    // Fetch the story details using Shortcut API
+                    const story = await axios.get(`https://api.app.shortcut.com/api/v3/stories/${story_id}`, {
+                    headers: {
+                      "Shortcut-Token": SHORTCUT_API_TOKEN,
+                      "Content-Type": "application/json"
+                    
+                    }
+                   
+                    });
+                    
+                    story_owners = story.data.owner_ids
+                    discord_mentions = story_owners.map(owner => `<${users.get(owner)}>`);
+
+                    const comment_message = `${discord_mentions.join(', ')} New comment on [${story_name}](${story_url})`;
+                    const channel = await client.channels.fetch(DISCORD_CHANNEL_ID);
+                    channel.send(comment_message);
+    
+                    logger.info(`Sent message to Discord channel ${DISCORD_CHANNEL_ID}: ${comment_message}`);
+                    res.status(200).send('Event processed');
+
+                }
+            } catch (error) {
+                logger.error('Error processing event:', error);
+                res.status(500).send('Internal Server Error');
+            }
+
+        }
 });
 
 // Start the Express server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  logger.info(`Server is listening on port ${PORT}`);
+    logger.info(`Server is listening on port ${PORT}`);
 });
 
 // Start the login process with retries
